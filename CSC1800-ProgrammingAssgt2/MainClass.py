da37fb1a31a3ef6665b4bd825aa00f876dfb3d8c
Frank Bonini <30274597+FBonini22@users.noreply.github.com>
2018-10-09 18:15:56 -0400

    Added family tree object, person object, input case checking. Added comments

--- CSC1800-ProgrammingAssgt2/MainClass.py
+++ CSC1800-ProgrammingAssgt2/MainClass.py
@@ -1,1 +1,44 @@
+#!Frank Bonini, Gabrielle Yuhas, Kaitlyn Krill
+
+#Todo: for-loop for file reading; or while loop
+
+
+#Get input properly
+INPUT = "X this input should be obtained properly, unlike this."
+
+#Split the input here. we will have to work with a split input in every of the below comparison statements
+SplittedList = INPUT.split()
+
+#Our person object. To have "instances" of this object, change the values in this dictionary, then .copy() it for use in anything else.
+person = {'name' : "", "parents" : [], "children" : []}
+
+#The family tree. Dictionary of the "person" object, having the names as keys
+tree = {'name' : person}
+
+#Keep for reference
+#person['name'] = "John"
+#tree[person['name']] = person.copy()
+#person['name'] = "Joe"
+#tree[person['name']] = person.copy()
+
+
+
+#Condition for relationship comparison
+if(INPUT.startswith('X')):
+
+    print('placeholder')
+
+    print(SplittedList)
+    
+
+
+elif(INPUT.startswith('W')):
+    #Condition for relationship listing
+    print('placeholder')
+
+else:
+    #Condition for adding relationships
+    print('placeholder')
+    
+
 
