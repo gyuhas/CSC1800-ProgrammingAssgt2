b141921cdb80d3f73e0f9c4800abd68035f47680
gyuhas <GYuhas@urbn.com>
2018-10-31 12:01:20 -0400

    Added Cousin and Parents

--- CSC1800-ProgrammingAssgt2/MainClass.py
+++ CSC1800-ProgrammingAssgt2/MainClass.py
@@ -1,7 +1,35 @@
 #!Frank Bonini, Gabrielle Yuhas, Kaitlyn Krill
+#!/usr/lib/python
+import sys
+
+def parentCheck(name1, name2):
+    if (not name1 in tree or not name2 in tree):
+        return False
+    if (name1 not in tree.get(name2)[0]):
+        return False
+    return True
+
+def cousinCheck(name1, name2):
+    if (not name1 in tree or not name2 in tree):
+        return False
+
+    if (name1 == name2):
+        return False
+
+    i = 0
+    isCousin = False
+
+    while (i < len(tree) and not isCousin):
+        pName = listOfNames[i]
+
+        if (ancestorCheck(pName, name1) and ancestorCheck(pName, name2)):
+            isCousin = True
+
+        i += 1
+
+    return isCousin
 
 #Todo: for-loop for file reading; or while loop
-
 
 #Get input properly
 INPUT = "X this input should be obtained properly, unlike this."
